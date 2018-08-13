98f151ae9d3601fc53e94eb53cc51e73b55f4953
Debamitro Chakraborti <debamitro@gmail.com>
2018-08-13 16:15:38 -0400

    Could connect button click

--- src/generate_bangla_utf8_cljs/core.cljs
+++ src/generate_bangla_utf8_cljs/core.cljs
@@ -1,8 +1,26 @@
 (ns generate-bangla-utf8-cljs.core
   )
 
-(js/console.log "Hah")
+(defn handle-convert-request
+  []
+  (js/console.log "clicked convert")
+  )
 
+(defn attach-events
+  []
+  (.addEventListener
+   (js/document.querySelector "#convertButton")
+   "click"
+   handle-convert-request
+   )
+  )
+
+(js/ready attach-events)
+
+
+;; (js/console.log "Hah")
+
+;; This is for nodejs
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
