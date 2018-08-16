ed7d05c5c529b6ac319c4f92ccfd9e5e722e782f
Debamitro Chakraborti <debamitro@gmail.com>
2018-08-16 17:56:03 -0400

    updated test

--- test/generate_bangla_utf8_cljs/core_test.clj
+++ test/generate_bangla_utf8_cljs/core_test.clj
@@ -3,8 +3,15 @@
             [generate-bangla-utf8-cljs.converter :as converter]))
 
 (deftest a-test
-  (testing "FIXME, I fail."
+  (testing "Try a phrase"
     (= (converter/to-bangla-utf8 "ro^de rAngA i^Ter pA^jA") "রোঁদে রাঙা ইঁটের পাঁজা")))
+(deftest a-test2
+  (testing "Try a phrase"
+   (= (converter/to-bangla-utf8 "hA^u mA^u khA^u") "হাঁউ মাঁউ খাঁউ")))
+
+(deftest b-test
+  (testing "Try a word"
+    (= (converter/to-bangla-utf8 "nidArun garam") "নিদারুন গরম")))
 
 (defn -main
   "I don't do a whole lot ... yet."
