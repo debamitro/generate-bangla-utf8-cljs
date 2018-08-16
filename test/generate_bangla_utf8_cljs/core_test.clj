4b5e5ebb376b27a1ec876358ef5ad379cb6a4afd
Debamitro Chakraborti <debamitro@gmail.com>
2018-08-16 11:57:28 -0400

    added unit test, parinfer formatting changed some stuff

--- test/generate_bangla_utf8_cljs/core_test.clj
+++ test/generate_bangla_utf8_cljs/core_test.clj
@@ -1,7 +1,11 @@
 (ns generate-bangla-utf8-cljs.core-test
   (:require [clojure.test :refer :all]
-            [generate-bangla-utf8-cljs.core :refer :all]))
+            [generate-bangla-utf8-cljs.converter :as converter]))
 
 (deftest a-test
   (testing "FIXME, I fail."
-    (is (= 0 1))))
+    (= (converter/to-bangla-utf8 "ro^de rAngA i^Ter pA^jA") "রোঁদে রাঙা ইঁটের পাঁজা")))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args])
